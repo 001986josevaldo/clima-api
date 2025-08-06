3397c2267e7a3327548bd7cb733433b395e7ef2e
001986josevaldo <josevaldo.m@unemat.br>
2025-08-06 19:30:45 -0400

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
+# app.py
 from flask import Flask, request, jsonify
-import requests
-import os
-from dotenv import load_dotenv
 from flask_cors import CORS
+from dotenv import load_dotenv
+from ClimaController import ClimaController
 
 load_dotenv()
-# para executar
-# python3.10 app.py
 
 app = Flask(__name__)
-CORS(app)  # <-- habilita CORS para todos os domínios
-
+CORS(app)
 
 @app.route('/clima')
 def clima_por_cep():
@@ -23,35 +20,13 @@ def clima_por_cep():
     cep = cep.replace("-", "").strip()
 
     try:
-        # Etapa única: Endereço via ViaCEP
-        res_cep = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
-        print("Resposta do ViaCEP:", res_cep.text)
-
-        if res_cep.status_code != 200:
-            return jsonify({"erro": "Erro ao buscar CEP"}), 500
-
-        endereco = res_cep.json()
-        if "erro" in endereco:
-            return jsonify({"erro": "CEP inválido"}), 400
-
-        # Criar retorno somente com os dados que chegaram
-        resultado = {
-            "cep": endereco.get("cep"),
-            "logradouro": endereco.get("logradouro"),
-            "bairro": endereco.get("bairro"),
-            "cidade": endereco.get("localidade"),
-            "uf": endereco.get("uf"),
-            "ddd": endereco.get("ddd"),
-            "siafi": endereco.get("siafi"),
-            "ibge": endereco.get("ibge")
-        }
-
+        controller = ClimaController()
+        resultado = controller.buscar(cep)
         return jsonify(resultado)
 
     except Exception as e:
         print("Erro inesperado:", str(e))
         return jsonify({"erro": "Erro no processamento", "detalhes": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
